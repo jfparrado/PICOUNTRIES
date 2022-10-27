47269302262163bd4d97c02acc969fc365c78021
juan parrado <jnsparrado@hotmail.com>
2022-10-27 10:09:42 -0300

    models created

--- PI2/PI-Pokemon-main/api/src/models/Pokemon.js
+++ PI2/PI-Pokemon-main/api/src/models/Pokemon.js
@@ -1,12 +1,44 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('pokemon', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
+  sequelize.define(
+    "pokemon",
+    {
+      id: {
+        type: DataTypes.UUID,
+        primaryKey: true,
+        allowNull: false,
+        defaultValue: UUIDV1, // para que pongan los ids automaticamente la v1 crea los ids usando la fecha y hora de creacion
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      hp: {
+        type: DataTypes.INTEGER,
+      },
+      attack: {
+        //fuerza
+        type: DataTypes.INTEGER,
+      },
+      defense: {
+        type: DataTypes.INTEGER,
+      },
+      speed: {
+        type: DataTypes.INTEGER,
+      },
+      heigth: {
+        type: DataTypes.INTEGER,
+      },
+      weight: {
+        type: DataTypes.INTEGER,
+      },
     },
-  });
+    {
+      createdAt: false,
+      updatedAt: false,
+    }
+  );
 };
